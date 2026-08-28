7f647017f96b406a4c040a7b176668df98f6a671
Janner Escobar <jannerescobar123@gmail.com>
2026-08-28 09:06:22 -0500

    chore: initialize backend project

---
+++ src/main/java/com/carvajal/wishlist/CarvajalWishlistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carvajal.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarvajalWishlistBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarvajalWishlistBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carvajal/wishlist/CarvajalWishlistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carvajal.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarvajalWishlistBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
