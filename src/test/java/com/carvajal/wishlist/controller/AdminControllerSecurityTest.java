070db1469ff32cc8bd307e1805a7d32c12e66e20
MaiVer4 <ll3925430@gmail.com>
2026-09-11 14:32:16 -0500

    fix: correct WebMvcTest imports for spring boot 3.x

--- src/test/java/com/carvajal/wishlist/controller/AdminControllerSecurityTest.java
+++ src/test/java/com/carvajal/wishlist/controller/AdminControllerSecurityTest.java
@@ -8,7 +8,7 @@ import com.carvajal.wishlist.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
